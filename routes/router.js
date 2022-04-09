a6adf81678321a38a7797f808b9f47ba62681d6a
monkey-king-jim <57361690+monkey-king-jim@users.noreply.github.com>
2022-04-09 11:05:55 -0400

    creating routes

---
+++ routes/router.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+
+const bcrypt = require("bcryptjs");
+const uuid = require("uuid");
+const jwt = require("jsonwebtoken");
+
+const db = require("../lib/db.js");
+const userMiddleware = require("../middleware/users.js");
+router.post("/sign-up", (req, res, next) => {});
+router.post("/login", (req, res, next) => {});
+router.get("/secret-route", (req, res, next) => {
+  res.send("This is the secret content.");
+});
+module.exports = router;
