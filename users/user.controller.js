ac51e7025c6b834a417741aae776eb13010f5cef
monkey-king-jim <57361690+monkey-king-jim@users.noreply.github.com>
2022-04-28 15:26:23 -0400

    Update user.controller.js

--- users/user.controller.js
+++ users/user.controller.js
@@ -2,8 +2,8 @@ const express = require("express");
 const router = express.Router();
 const authorize = require("_middleware/authorize");
 const userService = require("./user.service");
-const Joi = require('joi');
-const validateRequest = require('_middleware/validate-request');
+const Joi = require("joi");
+const validateRequest = require("_middleware/validate-req");
 
 router.post("/login", loginSchema, login);
 router.post("/sign-up", signupSchema, signup);
@@ -17,9 +17,49 @@ module.exports = router;
 
 function loginSchema(req, res, next) {
   const schema = Joi.object({
-      username: Joi.string().required(),
-      email: Joi.string().required();
+    loginInfo: Joi.string().required(),
     password: Joi.string().required(),
   });
   validateRequest(req, next, schema);
+}
+
+function login(req, res, next) {
+  userService
+    .login(req.body)
+    .then((user) => res.json(user))
+    .catch(next);
+}
+
+function signupSchema(req, res, next) {
+  const schema = Joi.object({
+    username: Joi.string().required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().min(8).required(),
+  });
+  validateRequest(req, next, schema);
+}
+
+function signup(req, res, next) {
+  userService
+    .create(req.body)
+    .then(() => res.json({ message: "Sign up successfully" }))
+    .catch(next);
+}
+
+function getAll(req, res, next) {
+  userService
+    .getAll()
+    .then((users) => res.json(users))
+    .catch(next);
+}
+
+function getCurrent(req, res, next) {
+  res.json(req.user);
+}
+
+function getById(req, res, next) {
+  userService
+    .getById(req.params.id)
+    .then((user) => res.json(user))
+    .catch(next);
 }
