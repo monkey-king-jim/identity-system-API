a8ddb9e2537ab8a6e36dd52935f2fb69d2e0dfc4
monkey-king-jim <57361690+monkey-king-jim@users.noreply.github.com>
2022-04-28 16:32:47 -0400

    modify starter server

--- index.js
+++ index.js
@@ -1,29 +1,19 @@
+require("rootpath")();
 const express = require("express");
 const app = express();
+const cors = require("cors");
+const errorHandler = require("_middleware/error-handler");
 
-const sequelize = require("./model/user");
-
-const PORT = process.env.PORT || 3000;
-
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-// app.use(cors());
+app.use(cors());
 
-const router = require("./routes/router.js");
-app.use("/auth", router);
+// api
+app.use("/users", require("./users/users.controller"));
 
-app.use((error, req, res, next) => {
-  const status = error.status || 500;
-  const message = error.message;
-  const data = error.data;
-  console.log(res);
-  res.status(status).json({ message: message, data: data });
-});
+// global error handler
+app.use(errorHandler);
 
-sequelize
-  .sync()
-  .then((result) => {
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+// start server
+const port = process.env.PORT;
+app.listen(port, () => console.log("Server listening on port " + port));
