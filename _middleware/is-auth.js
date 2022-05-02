81d00ebb35da239ca80926deeced04d0acb1b4a6
monkey-king-jim <57361690+monkey-king-jim@users.noreply.github.com>
2022-05-02 14:30:22 -0400

    Update is-auth.js

--- _middleware/is-auth.js
+++ _middleware/is-auth.js
@@ -4,7 +4,12 @@ const SECRET = process.env.SECRET;
 
 module.exports = isAuth;
 
-function isAuth() {
+function isAuth(roles = []) {
+  // param can be a string or array
+  // e.g. 'User'/ Role.User/ [Role.Admin, Role.User]
+
+  if (typeof roles === "string") roles = [roles];
+
   return [
     // auth jwt token
     jwt({ secret: SECRET, algorithms: ["HS256"] }),
@@ -13,10 +18,11 @@ function isAuth() {
       // user ID contained in subject property in token
       const user = await db.User.findByPk(req.auth.subject);
 
-      if (!user) return res.status(401).json({ message: "Unauthorzied User!" });
+      if (!user || (roles.length && !roles.includes(account.role)))
+        return res.status(401).json({ message: "Unauthorzied User!" });
 
       // auth succeed and attach user instance got by ID to req.user
-      req.user = user.get();
+      req.user.role = user.role;
       next();
     },
   ];
